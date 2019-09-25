ec62e5b0f678bfaf1ef7f8287cbc8230f9e9b4fe
Filippo Vicentini <filippovicentini@gmail.com>
2019-09-25 17:51:13 +0200

    With Metropolis sample do more than one switch per pass

--- src/Samplers/MCMCRules/Metropolis.jl
+++ src/Samplers/MCMCRules/Metropolis.jl
@@ -32,22 +32,25 @@ function markov_chain_step!(σ, s::MCMCSampler{Metropolis}, net::Union{MatrixNet
     # the denominator
     logψ_σ = 2*real(net(σ))
 
-    # 1- Flip a spin
-    sites_to_flip = 1:nsites(σ)
-    flipat = rand(c.rng, sites_to_flip) #1:nsites(σ))
-    old_val, new_val =flipat!(c.rng, σ, flipat)
+    for i=1:s.rule.num_switches
+        # 1- Flip a spin
+        sites_to_flip = 1:nsites(σ)
+        flipat = rand(c.rng, sites_to_flip)
+        old_val, new_val =flipat!(c.rng, σ, flipat)
 
-    # 2- Compute the probability to be in that state
-    logψ_σp = 2*real(net(σ)) #log_prob_ψ(net, σ)
-    log_ratio = logψ_σp - logψ_σ
-    prob = exp(log_ratio)
-    rv = rand(c.rng)
-    # If the move is rejected, revert it
-    if !(rv < prob)
-        setat!(σ, flipat, old_val)
-    else
-        #logψ_σ = 2*real(net(σ)) #log_prob_ψ(net, σ, alg_cache)
-        c.steps_accepted += 1
+        # 2- Compute the probability to be in that state
+        logψ_σp = 2*real(net(σ)) #log_prob_ψ(net, σ)
+        log_ratio = logψ_σp - logψ_σ
+        prob = exp(log_ratio)
+        rv = rand(c.rng)
+
+        # If the move is rejected, revert it
+        if !(rv < prob)
+            setat!(σ, flipat, old_val)
+        else # else accept it
+            logψ_σ = logψ_σp
+            c.steps_accepted += 1
+        end
     end
     c.steps_done += 1
 
@@ -55,21 +58,24 @@ function markov_chain_step!(σ, s::MCMCSampler{Metropolis}, net::Union{MatrixNet
 end
 
 function markov_chain_step!(σ::LUState, s::MCMCSampler{Metropolis}, net::Union{MatrixNet,KetNet}, c)
-    # 1- Flip a spin
-    sites_to_flip = 1:nsites(σ)
-    flipat = rand(c.rng, sites_to_flip) #1:nsites(σ))
-    old_val, new_val = flipat_fast!(c.rng, σ, flipat)
+    # Switch num_switches times
+    for i=1:s.rule.num_switches
+        # 1- Flip a spin
+        sites_to_flip = 1:nsites(σ)
+        flipat = rand(c.rng, sites_to_flip) #1:nsites(σ))
+        old_val, new_val = flipat_fast!(c.rng, σ, flipat)
 
-    # 2- Compute the probability to be in that state
-    log_ratio = 2*real(logψ_Δ(net, σ))
-    prob = exp(log_ratio)
-    rv = rand(c.rng)
-    # If the move is rejected, revert it
-    if !(rv < prob)
-        clear_changes!(σ)
-    else
-        update_lut!(σ, net)
-        c.steps_accepted += 1
+        # 2- Compute the probability to be in that state
+        log_ratio = 2*real(logψ_Δ(net, σ))
+        prob = exp(log_ratio)
+        rv = rand(c.rng)
+        # If the move is rejected, revert it
+        if !(rv < prob)
+            clear_changes!(σ)
+        else
+            update_lut!(σ, net)
+            c.steps_accepted += 1
+        end
     end
     c.steps_done += 1
 
