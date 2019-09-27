875cd6f42169ff924650b1cce3e303c6cc8b4d99
Filippo Vicentini <filippovicentini@gmail.com>
2019-09-27 18:57:51 +0200

    Remove redundant

--- src/Algorithms/SR/SR_eval.jl
+++ src/Algorithms/SR/SR_eval.jl
@@ -1,42 +1,22 @@
-## Matrix whole space
-function sample_network_wholespace!(
-  res::MCMCSREvaluationCache,
-  prob::HermitianMatrixProblem,
-  net, σ)
-
-  lnψ, ∇lnψ = logψ_and_∇logψ!(res.∇lnψ, net, σ)
-  E         = compute_Cloc(prob, net, σ, lnψ, res.σ)
-
-  prob = exp(2*real(lnψ))
-  res.Zave   += prob
-  res.Eave   += prob  * E
-  push!(res.Evalues, prob*E)
-
-  for (i, _∇lnψ)= enumerate(∇lnψ.tuple_all_weights)
-    res.Oave[i]  .+= prob .* _∇lnψ
-    res.OOave[i] .+= prob .* conj.(_∇lnψ) .* transpose(_∇lnψ)
-    res.EOave[i] .+= prob .* conj.(_∇lnψ) .* E
-  end
-  return res
-end
 
 ## Matrix sampled
 function sample_network!(
   res::MCMCSREvaluationCache,
   prob::HermitianMatrixProblem,
-  net, σ)
+  net, σ, wholespace=false)
 
   lnψ, ∇lnψ = logψ_and_∇logψ!(res.∇lnψ, net, σ)
   E         = compute_Cloc(prob, net, σ, lnψ, res.σ)
 
+  prob        = wholespace ? exp(2*real(lnψ)) : 1.0
   res.Eave   += E
-  res.Zave   += 1.0 #exp(2*real(lnψ))
+  res.Zave   += prob #1.0 #exp(2*real(lnψ))
   push!(res.Evalues, E)
 
   for (i,_∇lnψ)=enumerate(∇lnψ.tuple_all_weights)
-    res.Oave[i]  .+= _∇lnψ
-    res.OOave[i] .+= conj.(_∇lnψ) .* transpose(_∇lnψ)
-    res.EOave[i] .+= conj.(_∇lnψ) .* E
+    res.Oave[i]  .+= prob .* _∇lnψ
+    res.OOave[i] .+= prob .* conj.(_∇lnψ) .* transpose(_∇lnψ)
+    res.EOave[i] .+= prob .* conj.(_∇lnψ) .* E
   end
   return res
 end
