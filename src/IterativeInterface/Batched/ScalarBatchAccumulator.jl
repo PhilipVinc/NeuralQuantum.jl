4a764aaf8bef0899b269386a300114e83bd401aa
Filippo Vicentini <filippovicentini@gmail.com>
2019-10-07 15:13:16 +0200

    docs

--- src/IterativeInterface/Batched/ScalarBatchAccumulator.jl
+++ src/IterativeInterface/Batched/ScalarBatchAccumulator.jl
@@ -1,14 +1,32 @@
-mutable struct ScalarBatchAccumulator{N,A,B,C,D}
-    bnet::N
+"""
+    ScalarBatchAccumulator(net, state, batch_size)
 
-    in_buf::A       # the matrix of Nsites x batchsz used as state
-    out_buf::B
+A ScalarBatchAccumulator is used to evaluate the contribution to
+local observables ⟨σ|Ô|σ'⟩ψ(σ')/ψ(σ) , but by computing the neural
+network ψ(σ) in batches of size `batch_size`. This is essential
+to extrract a speedup when using GPUs.
+
+This is an internal implementation detail of NeuralQuantum, and should
+not be relied upon.
+
+Once constructed, a ScalarBatchAccumulator is supposed to be used as follows:
+- if `isfull(sba) == true` you should not push new elements to it (an error
+will be throw otherwise)
+- data is pushed as `sba(⟨σ|Ô|σ'⟩, σ', ψ(σ))`.
+The configuration should be passed as a vector (if ket) or as a tuple
+of two vectors (if density matrix).
+"""
+mutable struct ScalarBatchAccumulator{N,A,B,C,D}
+    bnet::N         # A batched version of the cached neural network
+
+    in_buf::A       # the matrix of Nsites x batchsz used as input
+    out_buf::B      # The row vector of outputs
 
     ψ0_buf::C       # Buffers alls the <σ|ψ> of the denominator
     mel_buf::D      # ⟨σ|Ô|σ'⟩ in the buffer
 
     buf_n::Int      # Counter for elements in buffer
-    batch_sz::Int
+    batch_sz::Int   # batch size
 end
 
 function ScalarBatchAccumulator(net::NeuralNetwork, v::State, batch_sz)
@@ -29,14 +47,22 @@ end
 
 
 Base.length(a::ScalarBatchAccumulator) = a.batch_sz
+Base.count(a::ScalarBatchAccumulator)  = a.buf_n
+isfull(a::ScalarBatchAccumulator) = count(a) == length(a)
 
-isfull(a::ScalarBatchAccumulator) = a.buf_n == length(a)
+"""
+    init!(c::ScalarBatchAccumulator)
 
+Resets the internal counter of the accumulator, deleting
+all previously accumulated (but not computed) values.
+"""
 init!(c::ScalarBatchAccumulator) = c.buf_n = 0
 
 function (c::ScalarBatchAccumulator)(mel, v, ψ0)
+    @assert !isfull(c) "Pushed data to a full accumulator."
+
     # Increase the step in our internal buffer
-    # this is guaranteed to always be < max_capacity
+    # this should be guaranteed to always be < max_capacity
     c.buf_n = c.buf_n + 1
 
     c.ψ0_buf[c.buf_n]   = ψ0
@@ -44,6 +70,20 @@ function (c::ScalarBatchAccumulator)(mel, v, ψ0)
     store_state!(c.in_buf, v, c.buf_n)
 end
 
+"""
+    process_accumulator!(c)
+
+Processes all states stored in the accumulator, by computing their
+relative local contribution.
+
+It is safe to call this even if the accumulator is not full. In this
+case all data beyond the count should be disregarded as it was
+not initialized.
+
+The output will be returned. You should not assume ownership of
+the output, as it is preallocated and will be used for further
+computations of the accumulator.
+"""
 function process_accumulator!(c::ScalarBatchAccumulator)
     out_buf  = logψ!(c.out_buf, c.bnet, c.in_buf)
     out_buf .-= c.ψ0_buf
@@ -51,8 +91,7 @@ function process_accumulator!(c::ScalarBatchAccumulator)
     #collect ? if using the gpu... need to think about this
 
     out_buf .*= c.mel_buf
-
-    c.buf_n = 0
-
+    init!(c)
+    
     return out_buf
 end
