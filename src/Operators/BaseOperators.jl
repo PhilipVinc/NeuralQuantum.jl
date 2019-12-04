86b0bcf7969ccf8e5b27158428229e5724aece6e
Filippo Vicentini <filippovicentini@gmail.com>
2019-12-04 11:56:37 +0100

    remove state 2

--- src/Operators/BaseOperators.jl
+++ src/Operators/BaseOperators.jl
@@ -1,7 +1,7 @@
 abstract type AbsLinearOperator end
 
 """
-    row_valdiff(op::AbsLinearOperator, v::State) -> OpConnection
+    row_valdiff(op::AbsLinearOperator, v) -> OpConnection
 
 Returns all non-zero elements in the row represented by the state `v` of
 operator `op`. The result is an `OpConnection` type.
@@ -9,14 +9,14 @@ operator `op`. The result is an `OpConnection` type.
 row_valdiff(op::AbsLinearOperator, v) = row_valdiff!(OpConnection(op), op, v)
 
 """
-    row_valdiff!(opconn::OpConnection, op::AbsLinearOperator, v::State)
+    row_valdiff!(opconn::OpConnection, op::AbsLinearOperator, v)
 
 Returns all non-zero elements in the row represented by the state `v` of
 operator `op` by mutating in-place the `OpConnection` object `opconn`
 """
 function row_valdiff! end
 
-row_valdiff_index(op::AbsLinearOperator, v::State) = row_valdiff_index!(OpConnectionIndex(op), op, v)
+#row_valdiff_index(op::AbsLinearOperator, v::State) = row_valdiff_index!(OpConnectionIndex(op), op, v)
 function row_valdiff_index! end
 
 # standard functions
