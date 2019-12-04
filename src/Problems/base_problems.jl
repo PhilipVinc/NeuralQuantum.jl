89fe04527cab5488e959b5597fd47cebf8899d04
Filippo Vicentini <filippovicentini@gmail.com>
2019-12-04 13:25:44 +0100

    fix tests

--- src/Problems/base_problems.jl
+++
@@ -1,5 +0,0 @@
-abstract type AbstractSteadyStateProblem <: AbstractProblem end
-abstract type HermitianMatrixProblem <: AbstractSteadyStateProblem end
-abstract type LRhoSquaredProblem <: AbstractSteadyStateProblem end
-abstract type OpenTimeEvolutionProblem <: AbstractSteadyStateProblem end
-abstract type OperatorEstimationProblem <: AbstractProblem end
