33975e1253aff4acda14caf42bf123cbb333fa14
Filippo Vicentini <filippovicentini@gmail.com>
2019-12-06 12:25:25 +0100

    Add new SR solvers

--- src/Algorithms/SR/SR.jl
+++ src/Algorithms/SR/SR.jl
@@ -1,21 +1,29 @@
 export SR, sr_none, sr_multiplicative, sr_shift
-export sr_diag, sr_cholesky, sr_div, sr_minres, sr_lsq, sr_cg
+export sr_diag, sr_cholesky, sr_div, sr_minres, sr_lsq, sr_cg, sr_qlp
 
 @enum SRPreconditionType sr_none=1 sr_shift=2 sr_multiplicative=3
-@enum SRAlgorithm sr_diag=1 sr_cholesky=2 sr_pivcholesky=3 sr_div=4 sr_minres=11 sr_lsq=12 sr_cg=13
+@enum SRAlgorithm sr_diag=1 sr_cholesky=2 sr_pivcholesky=3 sr_div=4 sr_qlp=11 sr_minres=12 sr_lsq=14 sr_cg=15
 is_iterative(alg::SRAlgorithm) = Int(alg) >=10
 
 ################################################################################
 ######   Preconditioning algorithm definition (structure holding params)  ######
 ################################################################################
 """
-    SR([use_iterative=true, ϵ=0.001, λ0=100, b=0.95, λmin=1e-4, precondition_type=sr_shift)
+    SR([use_iterative=true, ϵ=0.001, λ0=100, b=0.95, λmin=1e-4, [precondition_type=sr_shift, algorithm=sr_qlp])
 
 Stochastic Reconfiguration preconditioner which corrects the gradient according
 to the natural gradient computed as S^-1 ∇C. Using this algorithm will lead to
 the computation of the S matrix together with the gradient of the cost function
 ∇C. To compute the natural gradient S^-1∇C an iterative scheme (Minres-QLP) or
 a direct inversion is used.
+
+The linear system x = S^-1 ∇C is by default solved with `minres_qlp` iterative
+solver. Alternatively you can use `sr_minres`, `sr_cg` or `sr_lsq` to use respectively
+the minres, conjugate gradient and least square solvers from IterativeSolvers.jl.
+For small systems you can also solve it by computing the pseudo-inverse (`sr_diag`),
+the cholesky factorisation (`sr_cholesky`) the pivoted-cholesky factorisation (`sr_pivcholesky`),
+and using the automatic julia solver, usually involving qr decomposition (`sr_div`).
+Those non-iterative methods are all from Base.LinearAlgebra.
 
 If `use_iterative=true` the inverse matrix `S^-1` is not computed, and an iterative
 MINRES-QLP algorithm is used to compute the product S^-1*F
--- src/Algorithms/SR/SR_batched.jl
+++ src/Algorithms/SR/SR_batched.jl
@@ -6,13 +6,13 @@ struct SRDirectCache{Tc,T,G,E}
 end
 
 function _sr_direct_cache(algo::SR, prob, net)
-    T  = eltype(trainable_first(net))
-    g  = grad_cache(T, net)
+    T = eltype(trainable_first(net))
+    g = grad_cache(T, net)
     gv = vec_data(g)
 
-    S  = [similar(gv, T, length(gv), length(gv)) for gv=vec_data(g)]
+    S = [similar(gv, T, length(gv), length(gv)) for gv in vec_data(g)]
     if T <: Real
-        Sc = [similar(gv, Complex{T}, length(gv), length(gv)) for gv=vec_data(g)]
+        Sc = [similar(gv, Complex{T}, length(gv), length(gv)) for gv in vec_data(g)]
     else # is real
         Sc = S
     end
@@ -21,20 +21,20 @@ function _sr_direct_cache(algo::SR, prob, net)
 end
 
 function setup_algorithm!(g::SRDirectCache, ∇C, Ô)
-    O =  Ô
-    for (Sc, S, F)=zip(g.S_c, g.S, vec_data(g.F))
+    O = Ô
+    for (Sc, S, F) in zip(g.S_c, g.S, vec_data(g.F))
         T = eltype(S)
-        N = size(Ô,2)
+        N = size(Ô, 2)
 
         # TODO MPI Sync Sc
         mul!(Sc, O, O')
 
         if T <: Real
-            S  .= real.(Sc ./ N)
-            F  .= real.(∇C)
+            S .= real.(Sc ./ N)
+            F .= real.(∇C)
         else
             Sc ./= N
-            F  .= ∇C
+            F .= ∇C
         end
     end
 end
@@ -45,20 +45,22 @@ function precondition!(data::SRDirectCache, params::SR, iter_n)
 
     success = true
 
-    for (Δw, S, F) = zip(vec_data(data.∇out), data.S, vec_data(data.F))
+    for (Δw, S, F) in zip(vec_data(data.∇out), data.S, vec_data(data.F))
         # new matrix
         if params.precondition_type == sr_none
             Sprecond = S
         elseif params.precondition_type == sr_shift
             #Sprecond = S + convert(eltype(S), ϵ)*I
-            @inbounds @simd for i=1:size(S,1)
-                S[i,i] += convert(eltype(S), ϵ)
+            @inbounds @simd for i = 1:size(S, 1)
+                S[i, i] += convert(eltype(S), ϵ)
             end
             Sprecond = S
         elseif params.precondition_type == sr_multiplicative
-            λ0 = params.λ0; b = params.b; λmin = params.λmin;
-            λ = convert(eltype(S),max(λ0*b^iter_n, λmin))
-            Sprecond = S + λ*Diagonal(diag(S))
+            λ0 = params.λ0
+            b = params.b
+            λmin = params.λmin
+            λ = convert(eltype(S), max(λ0 * b^iter_n, λmin))
+            Sprecond = S + λ * Diagonal(diag(S))
         end
 
         @assert is_iterative(params) == false
@@ -66,7 +68,7 @@ function precondition!(data::SRDirectCache, params::SR, iter_n)
             Sinv = pinv(Sprecond)
             mul!(Δw, Sinv, F)
         elseif params.algorithm == sr_cholesky
-            C = cholesky!(Hermitian(S), check=true)
+            C = cholesky!(Hermitian(S), check = true)
             copyto!(Δw, F)
             ldiv!(C, Δw)
         elseif params.algorithm == sr_div
@@ -88,43 +90,93 @@ struct SRIterativeCache{Tc,T,G,E}
 end
 
 function _sr_iterative_cache(algo::SR, prob, net)
-    T  = eltype(trainable_first(net))
-    g  = grad_cache(T, net)
+    T = eltype(trainable_first(net))
+    g = grad_cache(T, net)
     gv = vec_data(g)
-    S  = [similar(gv, T, length(gv), length(gv)) for gv=vec_data(g)]
+    S = [similar(gv, T, length(gv), length(gv)) for gv in vec_data(g)]
     if T isa Real
-        Sc  = [similar(gv, Complex{T}, length(gv), length(gv)) for gv=vec_data(g)]
+        Sc = [similar(gv, Complex{T}, length(gv), length(gv)) for gv in vec_data(g)]
     else
         Sc = S
     end
     return SRIterativeCache(Sc, S, g, grad_cache(T, net))
 end
 
+function setup_algorithm!(g::SRIterativeCache, ∇C, Ô)
+    O = Ô
+    for (Sc, S, F) in zip(g.S_c, g.S, vec_data(g.F))
+        T = eltype(S)
+        N = size(Ô, 2)
+
+        # TODO MPI Sync Sc
+        mul!(Sc, O, O')
+
+        if T <: Real
+            S .= real.(Sc ./ N)
+            F .= real.(∇C)
+        else
+            Sc ./= N
+            F .= ∇C
+        end
+    end
+end
+
+
 function precondition!(data::SRIterativeCache, params::SR, iter_n)
     ϵ = params.sr_diag_shift
     success = true
 
-    for (Δw, S, F) = zip(vec_data(data.∇out), data.S, vec_data(data.F))
+    for (Δw, S, F) in zip(vec_data(data.∇out), data.S, vec_data(data.F))
         # new matrix
         if params.precondition_type == sr_none
             Sprecond = S
         elseif params.precondition_type == sr_shift
-            Sprecond = S + convert(eltype(S), ϵ)*I
+            Sprecond = S + convert(eltype(S), ϵ) * I
         elseif params.precondition_type == sr_multiplicative
-            λ0 = params.λ0; b = params.b; λmin = params.λmin;
+            λ0 = params.λ0
+            b = params.b
+            λmin = params.λmin
             #vv = eigvals(S)
             #println("regol $(max(λ0*b^iter_n, λmin)) --> $(minimum(vv)), a $(maximum(vv))")
-            λ = convert(eltype(S),max(λ0*b^iter_n, λmin))
-            Sprecond = S + λ*Diagonal(diag(S))
+            λ = convert(eltype(S), max(λ0 * b^iter_n, λmin))
+            Sprecond = S + λ * Diagonal(diag(S))
 
         end
 
-        if params.algorithm == sr_minres
-            x, hist = minresqlp(Sprecond, F, maxiter=size(S,2)*10, log=true, verbose=false, tol=params.sr_precision)
+        if params.algorithm == sr_qlp
+            x, hist = minresqlp(
+                Sprecond,
+                F,
+                maxiter = size(S, 2) * 10,
+                log = true,
+                verbose = false,
+                tol = params.sr_precision,
+            )
+        elseif params.algorithm == sr_minres
+            x, hist = minres(
+                Sprecond,
+                F,
+                maxiter = size(S, 2) * 10,
+                log = true,
+                verbose = false,
+                tol = params.sr_precision)
         elseif params.algorithm == sr_lsq
-            x, hist = lsqr(Sprecond, F, maxiter=size(S,2)*10, log=true, verbose=false, tol=params.sr_precision)
+            x, hist = lsqr(
+                Sprecond,
+                F,
+                maxiter = size(S, 2) * 10,
+                log = true,
+                verbose = false,
+            )
         elseif params.algorithm == sr_cg
-            x, hist = cg(Sprecond, F, maxiter=size(S,2)*10, log=true, verbose=false, tol=params.sr_precision)
+            x, hist = cg(
+                Sprecond,
+                F,
+                maxiter = size(S, 2) * 10,
+                log = true,
+                verbose = false,
+                tol = params.sr_precision,
+            )
         end
             #x, hist = cg(S.+ ϵ*I, F, maxiter=size(S,2)*10, log=true, verbose=true, tol=10e-10)
         if eltype(Δw) <: Real
@@ -135,7 +187,13 @@ function precondition!(data::SRIterativeCache, params::SR, iter_n)
         add_iters = 1
         while !hist.isconverged
             println("minresqlp not conerged. Additional $(size(S,2)*10) iters for the $add_iters time.")
-            x, hist = minresqlp(ΔW, Sprecond, F, maxiter=size(S,2)*10, log=true)
+            x, hist = minresqlp(
+                ΔW,
+                Sprecond,
+                F,
+                maxiter = size(S, 2) * 10,
+                log = true,
+            )
             Δw .= x
             add_iters += 1
             add_iters > 5 && break
