60d0b2bb4d64b4f409a678b6ff51887fcf52f3df
Filippo Vicentini <filippovicentini@gmail.com>
2020-03-04 23:23:41 +0100

    fix state iterator error and add tests (not an error)

--- test/Hilbert/space.jl
+++ test/Hilbert/space.jl
@@ -24,3 +24,27 @@ N = 4
     end
     @test all(vals .== 1:spacedimension(shilb))
 end
+
+function test_hilb_iterator(hilb)
+    it = states(hilb)
+
+    all_states = collect(it)
+
+    @test length(all_states) == spacedimension(hilb)
+    @test length(it) == spacedimension(hilb)
+    @test eltype(it) == typeof(state(hilb))
+
+    vals = zeros(length(all_states))
+    for (i,v) = enumerate(all_states)
+        vals[i] = index(hilb, v)
+    end
+
+    @test all(vals .== 1:spacedimension(hilb))
+end
+
+
+@testset "Hilbert space iterator" begin
+    test_hilb_iterator(HomogeneousSpin(N))
+    test_hilb_iterator(HomogeneousFock(N,2))
+    test_hilb_iterator(SuperOpSpace(HomogeneousSpin(N)))
+end
